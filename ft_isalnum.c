839dda790bb3a0197fec8838598ea8ecb9c5a873
fernns <43729148+fernnbr@users.noreply.github.com>
2025-07-27 18:19:46 -0300

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -2,9 +2,6 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57))
-	{
-		return (1);
-	}
-	return (0);
+	return (ft_isdigit(c) || ft_isalpha (c));
 }
+//Or you can solve not using the functions you designed before, by typing if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57))
