3bde2c53c2cfa38cfcace55c850a3ec4ebb4e9f0
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-05 22:33:50 -0300

    Create ft_is_alpha.c

---
+++ resilient-upgrade/ft_is_alpha.c
@@ -0,0 +1,14 @@
+int ft_isalpha(int c)
+{
+    if (c == EOF)
+        return 0;
+    if (c < 0 || c > 255)
+        return 0;
+  
+    c = (unsigned char)c;
+  
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        return 1;
+
+    return 0;
+}
