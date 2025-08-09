232269edd16bc1035b33de5620f25cd74ab6ab5e
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 16:40:16 -0300

    Update 04 - ft_isalpha.c

--- resilient-upgrade/04 - ft_isalpha.c
+++ resilient-upgrade/04 - ft_isalpha.c
@@ -3,14 +3,14 @@
 int ft_isalpha(int c)
 {
     if (c == EOF)
-        return 0;
+        return (0);
     if (c < 0 || c > 255)
-        return 0;
+        return (0);
   
     c = (unsigned char)c;
   
     if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-        return 1;
+        return (1);
 
-    return 0;
+    return (0);
 }
