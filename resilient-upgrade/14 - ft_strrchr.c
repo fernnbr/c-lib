276864472328a1d753d279dbd834ce4bd2ade583
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:08:15 -0300

    Update 14 - ft_strrchr.c

--- resilient-upgrade/14 - ft_strrchr.c
+++ resilient-upgrade/14 - ft_strrchr.c
@@ -1,1 +1,22 @@
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*last_occurrence;
+	unsigned char char_to_find;
+
+	if (!s)
+		return (NULL);
+	last_occurrence = NULL;
+	char_to_find = (unsigned char)c;
+	while (*s != '\0')
+	{
+		if ((unsigned char)*s == char_to_find)
+			last_occurrence = s;
+		s++;
+	}
+	if (char_to_find == '\0')
+		return ((char *)s);
+	return ((char *)last_occurrence);
+}
 
