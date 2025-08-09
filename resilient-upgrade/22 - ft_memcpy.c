c3505f9bb25081912b8d09e1bd94d821ccf84e29
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:30:18 -0300

    Update 22 - ft_memcpy.c

--- resilient-upgrade/22 - ft_memcpy.c
+++ resilient-upgrade/22 - ft_memcpy.c
@@ -1,1 +1,15 @@
+#include "libft.h"
 
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*ptrdest;
+	const unsigned char	*ptrsrc;
+
+	if (!dest && !src)
+		return (NULL);
+	ptrdest = (unsigned char *)dest;
+	ptrsrc = (const unsigned char *)src;
+	while (n--)
+		*ptrdest++ = *ptrsrc++;
+	return (dest);
+}
