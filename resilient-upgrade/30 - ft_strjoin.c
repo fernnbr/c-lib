1f12333deef8683daceedc129ca726140aa167a1
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:45:38 -0300

    Update 30 - ft_strjoin.c

--- resilient-upgrade/30 - ft_strjoin.c
+++ resilient-upgrade/30 - ft_strjoin.c
@@ -1,1 +1,38 @@
+#include "libft.h"
 
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*s3;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s3 = (char *)malloc(s1_len + s2_len + 1);
+	if (!s3)
+		return (NULL);
+
+	i = 0;
+	while (i < s1_len)
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < s2_len)
+	{
+		s3[i + j] = s2[j];
+		j++;
+	}
+	s3[i + j] = '\0';
+	return (s3);
+}
