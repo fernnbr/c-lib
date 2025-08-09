4d1bdc5d219022bbfb25cf4f4c154d0bf11744a7
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 16:57:28 -0300

    Delete 5.ft_isalnum.c

--- 5.ft_isalnum.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-int	ft_isalnum(int c)
-{
-	return (ft_isdigit(c) || ft_isalpha (c));
-}
-//Or you can solve not using the functions you designed before, by typing if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57))
