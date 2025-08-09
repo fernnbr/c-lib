7bfed760180de64c3857f69c60c5799db1f6b5b4
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:48:31 -0300

    Update 32 - ft_strmapi.c

--- resilient-upgrade/32 - ft_strmapi.c
+++ resilient-upgrade/32 - ft_strmapi.c
@@ -1,1 +1,23 @@
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*new_str;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	new_str = (char *)malloc(len + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_str[i] = (*f)((unsigned int)i, s[i]);
+		i++;
+	}
+	new_str[len] = '\0';
+	return (new_str);
+}
