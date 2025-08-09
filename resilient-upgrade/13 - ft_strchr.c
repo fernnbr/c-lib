afb3d63a206609721d0a067ebd2de7544eae3ba9
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 16:47:51 -0300

    Update 13 - ft_strchr.c

--- resilient-upgrade/13 - ft_strchr.c
+++ resilient-upgrade/13 - ft_strchr.c
@@ -1,1 +1,19 @@
+#include "libft.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = (char *)s;
+	i = 0;
+	while (ptr[i])
+	{
+		if (ptr[i] == (unsigned char)c)
+			return (&ptr[i]);
+		i++;
+	}
+	if (ptr[i] == '\0' && (unsigned char)c == '\0')
+		return (&ptr[i]);
+	return (0);
+}
