21e1bb2dbe5517500c69ebf1c19d324d4dffb334
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:37:39 -0300

    Update 26 - ft_calloc.c

--- resilient-upgrade/26 - ft_calloc.c
+++ resilient-upgrade/26 - ft_calloc.c
@@ -1,1 +1,18 @@
+#include "libft.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total;
+	void	*mem_alloc;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	total = nmemb * size;
+	if (total / nmemb != size)
+		return (NULL);
+	mem_alloc = malloc(total);
+	if (!mem_alloc)
+		return (NULL);
+	ft_bzero(mem_alloc, total);
+	return (mem_alloc);
+}
