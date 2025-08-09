20e20ae498e77036e765ee07cef853c3fb678b69
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 16:45:57 -0300

    Update 11 - ft_putnbr.c

--- resilient-upgrade/11 - ft_putnbr.c
+++ resilient-upgrade/11 - ft_putnbr.c
@@ -1,1 +1,29 @@
+#include "libft.h"
 
+int	ft_putchar_fd(char c, int fd)
+{
+	if (fd < 0)
+		return (-1);
+	if (write(fd, &c, 1) != 1)
+		return (-1);
+	return (0);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int	nbr;
+
+	if (fd < 0)
+		return ;
+
+	nbr = n;
+	if (nbr < 0)
+	{
+		if (ft_putchar_fd('-', fd) == -1)
+			return ;
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((nbr % 10) + '0', fd);
+}
