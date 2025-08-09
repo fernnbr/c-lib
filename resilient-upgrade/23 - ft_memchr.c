281564e66d7c35c834043c883e9084a45dbd8a06
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:32:33 -0300

    Update 23 - ft_memchr.c

--- resilient-upgrade/23 - ft_memchr.c
+++ resilient-upgrade/23 - ft_memchr.c
@@ -1,1 +1,21 @@
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char		c_char;
+	const unsigned char	*s_char;
+	size_t				i;
+
+	if (!s || n == 0)
+		return (NULL);
+	c_char = (unsigned char)c;
+	s_char = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (s_char[i] == c_char)
+			return ((void *)&s_char[i]);
+		i++;
+	}
+	return (NULL);
+}
