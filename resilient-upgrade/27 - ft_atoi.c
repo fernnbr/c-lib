f635987c223ac49bc0b10454054bca8b7eb623d2
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:40:15 -0300

    Update 27 - ft_atoi.c

--- resilient-upgrade/27 - ft_atoi.c
+++ resilient-upgrade/27 - ft_atoi.c
@@ -1,1 +1,26 @@
+#include "libft.h"
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	sign = 1;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	result = 0;
+	while (ft_isdigit(nptr[i]))
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
