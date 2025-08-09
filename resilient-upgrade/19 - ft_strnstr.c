629d1f896584b0682adfb175c7811dbb89408b2d
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:23:58 -0300

    Update 19 - ft_strnstr.c

--- resilient-upgrade/19 - ft_strnstr.c
+++ resilient-upgrade/19 - ft_strnstr.c
@@ -1,1 +1,23 @@
+#include "libft.h"
 
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!big || !little)
+		return (NULL);
+	if (*little == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len && little[j] != '\0')
+			j++;
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
+}
