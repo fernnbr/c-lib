fbaa472d070bbfe2e3122399f63bf14e2e556518
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:47:12 -0300

    Update 31 - ft_strtrim.c

--- resilient-upgrade/31 - ft_strtrim.c
+++ resilient-upgrade/31 - ft_strtrim.c
@@ -1,1 +1,22 @@
+#include "libft.h"
 
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	char	*s2;
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	s2 = (char *)malloc(end - start + 1);
+	if (!s2)
+		return (NULL);
+	ft_strlcpy(s2, s1 + start, end - start + 1);
+	return (s2);
+}
