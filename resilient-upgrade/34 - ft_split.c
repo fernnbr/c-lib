79adf73a57b5285360c27b3dc017c4fd6f3ef223
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:52:07 -0300

    Update 34 - ft_split.c

--- resilient-upgrade/34 - ft_split.c
+++ resilient-upgrade/34 - ft_split.c
@@ -1,1 +1,94 @@
+static size_t	count_words(const char *s, char c)
+{
+	size_t	count;
+	int		in_word;
 
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			count++;
+			in_word = 1;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	*copy_word(const char *start, size_t len)
+{
+	char	*word;
+	size_t	i;
+
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = start[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+/* free_split unchanged except for size_t for i */
+static void	free_split(char **array, size_t i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(array[i]);
+	}
+	free(array);
+}
+
+static int	extract_words(char **result, const char *s, char c)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (s[len] && s[len] != c)
+				len++;
+			result[i] = copy_word(s, len);
+			if (!result[i])
+			{
+				free_split(result, i);
+				return (0);
+			}
+			i++;
+			s += len;
+		}
+		else
+			s++;
+	}
+	result[i] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	size_t	words_count;
+
+	if (!s)
+		return (NULL);
+	words_count = count_words(s, c);
+	result = (char **)malloc((words_count + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	if (!extract_words(result, s, c))
+		return (NULL);
+	return (result);
+}
