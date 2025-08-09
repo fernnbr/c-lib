8104240b3937ea99cbe4834de775c8f38dd84bc7
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:35:01 -0300

    Update 24 - ft_memmove.c

--- resilient-upgrade/24 - ft_memmove.c
+++ resilient-upgrade/24 - ft_memmove.c
@@ -1,1 +1,32 @@
+#include "libft.h"
 
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptrdest;
+	const unsigned char	*ptrsrc;
+
+	if (!dest && !src)
+		return (NULL);
+	ptrdest = (unsigned char *)dest;
+	ptrsrc = (const unsigned char *)src;
+	if (ptrsrc < ptrdest && ptrdest < ptrsrc + n)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			ptrdest[i] = ptrsrc[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			ptrdest[i] = ptrsrc[i];
+			i++;
+		}
+	}
+	return (dest);
+}
