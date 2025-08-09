6c4eff01614481433adc8253bc7148528da515ee
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:26:51 -0300

    Update 21 - ft_memset.c

--- resilient-upgrade/21 - ft_memset.c
+++ resilient-upgrade/21 - ft_memset.c
@@ -1,1 +1,15 @@
+#include "libft.h"
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*dest;
+	unsigned char	c_char;
+
+	if (!s || n == 0)
+		return (s);
+	dest = (unsigned char *)s;
+	c_char = (unsigned char)c;
+	while (n--)
+		*dest++ = c_char;
+	return (s);
+}
