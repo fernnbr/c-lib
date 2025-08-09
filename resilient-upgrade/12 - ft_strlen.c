db7b954f92239e4050f1cbacd8e96fe54a81c93f
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 16:46:58 -0300

    Update 12 - ft_strlen.c

--- resilient-upgrade/12 - ft_strlen.c
+++ resilient-upgrade/12 - ft_strlen.c
@@ -1,1 +1,16 @@
+#include "libft.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	if (!s)
+		return (0);
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
