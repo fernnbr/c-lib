fcfad3ce228b6366256136b36f2af672deb31abd
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 17:11:16 -0300

    Update 15 - ft_strdup.c

--- resilient-upgrade/15 - ft_strdup.c
+++ resilient-upgrade/15 - ft_strdup.c
@@ -1,1 +1,18 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*dup;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, s, len);
+	dup[len] = '\0';
+	return (dup);
+}
 
