1160ab0560073f8b1b4616e4fc7dd36377f27f45
Shuri <43729148+fernnbr@users.noreply.github.com>
2025-08-09 19:36:12 -0300

    Update 01-main-isalpha.c

--- test-main/01-main-isalpha.c
+++ test-main/01-main-isalpha.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
+
+int main(void)
+{
+	int test_vals[] = {0, 31, 32, 65, 127, 128, -1, 255, 300};
+	int size = sizeof(test_vals) / sizeof(test_vals[0]);
+
+	for (int i = 0; i < size; i++)
+	{
+		int c = test_vals[i];
+		printf("ft_isascii(%d) = %d\n", c, ft_isascii(c));
+	}
+	return 0;
+}
+
+//Expected Result 
+
+0, 31, 32, 65, 127, 128, -1, 255, 300
 
